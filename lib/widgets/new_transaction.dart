2ea9d0a21bf0a309ec08cc33321a758edd6625db
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-08-18 08:31:07 +0530

    use single child scroll view for add transaction

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -30,8 +30,8 @@ class _NewTransactionState extends State<NewTransaction> {
     }
 
     // this.widget need to access parent class pointer method
-    this.widget.addNexTransaction(
-        _titleController.text, double.parse(_amountController.text),_selectedDate);
+    this.widget.addNexTransaction(_titleController.text,
+        double.parse(_amountController.text), _selectedDate);
 
     Navigator.of(context).pop();
   }
@@ -55,69 +55,76 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: _titleController,
-              onSubmitted: (_) => _submitData(),
-              /*onChanged: (value) {
-                        titleInput = value;
-                      },*/
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) =>
-                  _submitData(), // here _ means we ignore value here
-              //and pass it to arrow function
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+                /*onChanged: (value) {
+                          titleInput = value;
+                        },*/
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) =>
+                    _submitData(), // here _ means we ignore value here
+                //and pass it to arrow function
 
-              /*onChanged: (value) {
-                        amountInput = value;
-                      },*/
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                          ? 'No Date Chosen!'
-                          : 'Picked Date : ${DateFormat.yMd().format(_selectedDate!)}',
-                    ),
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      primary: Theme.of(context).primaryColor,
-                    ),
-                    onPressed: _presentDatePicker,
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                /*onChanged: (value) {
+                          amountInput = value;
+                        },*/
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Picked Date : ${DateFormat.yMd().format(_selectedDate!)}',
                       ),
                     ),
-                  ),
-                ],
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
+                      ),
+                      onPressed: _presentDatePicker,
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: _submitData,
-              style: ElevatedButton.styleFrom(
-                primary: Theme.of(context).primaryColor, //background
-                onPrimary:
-                    Theme.of(context).textTheme.button?.color, //font color
+              ElevatedButton(
+                onPressed: _submitData,
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor, //background
+                  onPrimary:
+                      Theme.of(context).textTheme.button?.color, //font color
+                ),
+                child: Text('Add Transaction'),
               ),
-              child: Text('Add Transaction'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
